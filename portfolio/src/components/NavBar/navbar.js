adf94c7f375f04e91511fb0ed5fb7ae16608761f
plmurdoch <pmurdoch@uvic.ca>
2024-07-14 20:53:54 -0700

    Initial commit

---
+++ portfolio/src/components/NavBar/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './navbar.css';
+import logo from '../../assets/logo.png';
+import {Link} from 'react-scroll';
+import contact from '../../assets/Contact.png';
+
+const Navbar = () => {
+    return(
+        <nav className='navbar'>
+            <img src={logo} alt="Logo" className='logo'/>
+            <div className ="desktopMenu">
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+                <Link className="desktopMenuListItem">Clients</Link>
+            </div>
+            <button className='desktopMenuBtn'>
+                <img src={contact} alt="contact" className="desktopMenuImg"/>Contact Me</button>
+        </nav>
+    )
+}
+export default Navbar;
